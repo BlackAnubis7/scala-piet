4206faacbd5bc26e224c2fa8152465c7040072db
Anubis <github@anubis.email>
2022-07-20 16:06:10 +0200

    format code according to scalafmt

--- src/main/scala/ImageReader.scala
+++ src/main/scala/ImageReader.scala
@@ -6,17 +6,17 @@ import javax.imageio.ImageIO
 import cases.RGB
 
 extension (img: BufferedImage)
-    def getPixel(x: Int, y: Int): RGB = 
-        val argb: Int = img.getRGB(x, y)
-        RGB (
-            (argb >> 16) & 0xff,
-            (argb >> 8)  & 0xff,
-            (argb)       & 0xff
-        )
+  def getPixel(x: Int, y: Int): RGB =
+    val argb: Int = img.getRGB(x, y)
+    RGB(
+      (argb >> 16) & 0xff,
+      (argb >> 8) & 0xff,
+      (argb) & 0xff
+    )
 
 class ImageReader(filePath: String):
-    private[this] val file: File = new File(filePath)
-    private[this] val image: BufferedImage = ImageIO.read(file)
+  private[this] val file: File = new File(filePath)
+  private[this] val image: BufferedImage = ImageIO.read(file)
 
-    def getPixel(x: Int, y: Int): RGB = image.getPixel(x, y)
+  def getPixel(x: Int, y: Int): RGB = image.getPixel(x, y)
 end ImageReader
